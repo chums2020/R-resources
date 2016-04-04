18698dc2b8d65fdaf441ea0792c36a4314c95695
Ying-Ting Lin <claire4621@gmail.com>
2016-04-04 01:41:47 -0500

    Update ui.R

--- Shiny/taipei-app/ui.R
+++ Shiny/taipei-app/ui.R
@@ -1,25 +1,25 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Cultural Actities in New Taipei City, updated daily"),
+  titlePanel("新北市政府文化局藝文活動 （每日更新）"),
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Select type of activities and date."),
+      helpText("輸入活動類型與日期後，右方表格會自動更新"),
       
-      radioButtons("type", label = h3("Please choose the type of activities you are interested in."),
-                   choices = list("Cultural Events Promotion", "exhibition info","Exhibition and Performing Arts", "call for submission/application",
-                                  "ticket/registration", "street artist", "community", "cultural development", "all"),
-                   selected = "all"),
+      radioButtons("type", label = h3("請選擇您感興趣的活動類型："),
+                   choices = list("藝文推廣", "藝術展演", "徵件/比賽/申請",
+                                  "索票/報名", "街頭藝人", "社區營造", "文化發展", "全部"),
+                   selected = "全部"),
       
       dateInput("date", 
-                label = h3("Please choose the date of your trip."), 
+                label = h3("請選擇日期"), 
                 value = as.character(Sys.Date())),
       
       br(),
       br(),
       
-      checkboxInput("period", "Do you allow for flexible dates? (+/- 7 days)", 
+      checkboxInput("period", "彈性時間 (包含選擇日期的前後7天)", 
                     value = TRUE)
     ),
     
