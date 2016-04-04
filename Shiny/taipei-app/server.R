cc530e587c9284b99f758a9a5a8e2ffa33cd8b23
Ying-Ting Lin <claire4621@gmail.com>
2016-04-04 01:35:59 -0500

    Update server.R

--- Shiny/taipei-app/server.R
+++ Shiny/taipei-app/server.R
@@ -3,14 +3,18 @@
 #given day, time, region, the app can look for activities that are going to take place
 
 
+#active url link
+#transform startdata, enddata
+
+
+
 library(jsonlite)
 library(curl)
 url = "http://data.ntpc.gov.tw/od/data/api/A97AEE33-4109-457B-9FB1-DB754A0BB100?$format=json"
 
 shinyServer(function(input, output) {
   jsonData <- fromJSON(url)
-  names(jsonData) <- c("公告機關","活動類型","開始時間","結束時間","活動名稱","網址","細節","公佈日期")
-  
+
   activityInput <- reactive({
     switch(input$type,
            "Cultural Events Promotion" = which(jsonData$type =="藝文推廣"),
@@ -21,22 +25,25 @@ shinyServer(function(input, output) {
            "street artist"= which(jsonData$type =="街頭藝人"),
            "community"= which(jsonData$type =="社區營造"),
            "cultural development"= which(jsonData$type =="文化發展"),
-           "null"= dim(jsonData)[1]
+           "all"= seq(1,dim(jsonData)[1])
     )
   })
-
+  
   adjust_dateInput <- reactive({
     if (input$period){
       return(seq.Date(as.Date(input$date)-7, as.Date(input$date)+7, by = "day"))
     }
     as.Date(input$date)
   })
-
+  
   
   output$table <- renderTable({
-    jsonData <- jsonData[activityInput(),]
-    jsonData <- jsonData[which(jsonData$startdata < max(adjust_dateInput())),]
-    jsonData <- jsonData[which(jsonData$enddata > min(adjust_dateInput())),]
-  })
+    jsonData <- jsonData[activityInput(),1:7]
+    jsonData <- jsonData[which(as.Date(jsonData$startdata) < max(adjust_dateInput())),]
+    jsonData <- jsonData[which(as.Date(jsonData$enddata) > min(adjust_dateInput())),]
+    names(jsonData) <- c("公告機關","活動類型","開始時間","結束時間","活動名稱","網址","細節")
+    jsonData <-data.frame(jsonData)
+  }, 
+  include.rownames=FALSE)
 })
 
