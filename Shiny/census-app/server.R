aea906785ef1f25636f699319d69df71d7e2213a
Ying-Ting Lin <claire4621@gmail.com>
2016-04-03 21:57:03 -0500

    Update server.R

--- Shiny/census-app/server.R
+++ Shiny/census-app/server.R
@@ -1,1 +1,23 @@
 
+library(quantmod)
+source("helpers.R")
+
+shinyServer(function(input, output) {
+  dataInput <- reactive({
+    getSymbols(input$symb, src = "yahoo", 
+               from = input$dates[1],
+               to = input$dates[2],
+               auto.assign = FALSE)
+  })
+  adjust_dataInput <- reactive({
+    if (input$adjust){
+      return(adjust(dataInput()))
+    }
+    dataInput()
+  })
+  output$plot <- renderPlot({
+    chartSeries(adjust_dataInput(), theme = chartTheme("white"), 
+      type = "line", log.scale = input$log, TA = NULL)
+  })
+  
+})
