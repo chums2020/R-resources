9556d7303ad1fe3a979961fc95524c14357ff1f5
Ying-Ting Lin <claire4621@gmail.com>
2016-04-08 02:21:34 -0500

    Update ui.R

--- Shiny/taipei-app/ui.R
+++ Shiny/taipei-app/ui.R
@@ -12,19 +12,15 @@ shinyUI(fluidPage(
                                   "索票/報名", "街頭藝人", "社區營造", "文化發展", "全部"),
                    selected = "全部"),
       
-      dateInput("date", 
+      dateRangeInput("dates", 
                 label = h3("請選擇日期"), 
-                value = as.character(Sys.Date())),
+                start = Sys.Date(), end =  Sys.Date()+7,
+                separator = " - ")
       
-      br(),
-      br(),
-      
-      checkboxInput("period", "彈性時間 (包含選擇日期的前後7天)", 
-                    value = TRUE)
     ),
     
     mainPanel(
-       DT::dataTableOutput('table')
+      DT::dataTableOutput('table')
       
     )
   )
