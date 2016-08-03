a794e17c563529e076f1d3e9205b10b3a45d86b4
Ying-Ting Lin <claire4621@gmail.com>
2016-08-03 14:50:49 +0800

    Update load_and_get.R

--- lappy/load_and_get.R
+++ lappy/load_and_get.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 # load & combine all .RData files in a directory
 data.files <- list.files(path="C:/Users/yingting/data", pattern="*.RData", full.names=T, recursive=FALSE)
 data.list <- lapply(data.files,load,.GlobalEnv) #load all .RData files in the list "data.files"
