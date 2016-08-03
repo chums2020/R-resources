766100a3ab2e56a8a69877e79724a711a2828600
Ying-Ting Lin <claire4621@gmail.com>
2016-08-03 14:50:19 +0800

    Update load_and_get.R

--- lappy/load_and_get.R
+++ lappy/load_and_get.R
@@ -1,7 +1,16 @@
+# load & combine all .RData files in a directory
 data.files <- list.files(path="C:/Users/yingting/data", pattern="*.RData", full.names=T, recursive=FALSE)
 data.list <- lapply(data.files,load,.GlobalEnv) #load all .RData files in the list "data.files"
 data.combined <-do.call(rbind, lapply(data.list, get)) # combine all .RData files in the list data.list into one data frame
 
+# load & combine all .csv files in a directory
+data.files <- list.files(path="C:/Users/yingting/data", pattern="*.csv", full.names=T, recursive=FALSE)
+data.list <- lapply(data.files, function(x) {
+  data <- read.csv(x, header= TRUE, sep=",", encoding = "UTF-8", stringsAsFactors = FALSE)
+  colnames(data) <-c("a", "b", "c", "d") # force the column names for consistency to avoid error in the do.call(rbind, .) process
+  data <- data[,1:4]
+})
+data.combined<-do.call(rbind, data.list)
 
 
 
